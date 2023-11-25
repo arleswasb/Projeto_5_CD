e4cf68130b11a17c9a42454121ada4eb3847378a
arleswasb <werbert.barradas.390@ufrn.edu.br>
2023-11-25 15:24:02 -0300

    CORREÇÃO DEMUX 1X6

--- arquivos VHDL/Demux_1x6.vhd
+++ arquivos VHDL/Demux_1x6.vhd
@@ -2,9 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity Demux_1x6 is -- entrada de valor teste para saida por moeda
-   Port ( Vt: in std_logic_vector(5 downto 0);
+   Port (ENT: in std_logic;
 			sel:  in std_logic_vector(2 downto 0);
-         saida0,saida1,saida2,saida3,saida4,saida5: out std_logic_vector(5 downto 0));
+        S5,S4,S3,S2,S1,S0: out std_logic);
 end Demux_1x6;
 
 architecture ckt of Demux_1x6 is
@@ -13,21 +13,21 @@ architecture ckt of Demux_1x6 is
 	begin
 			process (Vt,sel)
 			begin
-			saida0 <= "000000"; -- moeda de 1 centavo
-			saida1 <= "000000"; -- moeda de 5 centavos
-			saida2 <= "000000"; -- moeda de 10 centavos
-			saida3 <= "000000"; -- moeda de 25 centavos
-			saida4 <= "000000"; -- moeda de 50 centavos
-			saida5 <= "000000"; -- moeda de 100 centavos (1 real)
+			S0 <= '0'; -- moeda de 100 centavo (1 real)
+			S1 <= '0'; -- moeda de 50 centavos
+			S2 <= '0'; -- moeda de 25 centavos
+			S3 <= '0'; -- moeda de 15 centavos
+			S4 <= '0'; -- moeda de 10 centavos
+			S5 <= '0'; -- moeda de 1 centavos 
 			
 				case sel is
-					when "000" => saida0 <= Vt;
-					when "001" => saida1 <= Vt;
-					when "010" => saida2 <= Vt;
-					when "011" => saida3 <= Vt;
-					when "100" => saida4 <= Vt;
-					when "110" => saida5 <= Vt;
-					when others => saida0 <= "000000";
+					when "000" => S0 <= ENT;
+					when "001" => S1 <= ENT;
+					when "010" => S2 <= ENT;
+					when "011" => S3 <= ENT;
+					when "100" => S4 <= ENT;
+					when "110" => S5 <= ENT;
+					when others => S0 <= '0';
 				end case;
 			end process;
 end ckt;
