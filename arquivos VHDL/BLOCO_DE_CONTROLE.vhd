59217322f6905c68bb0dcbcabcf50f86ebde0164
arleswasb <werbert.barradas.390@ufrn.edu.br>
2023-11-25 14:09:18 -0300

    ATUALIZAÇÃO BLOCO DE CONTROLE

--- arquivos VHDL/BLOCO_DE_CONTROLE.vhd
+++ arquivos VHDL/BLOCO_DE_CONTROLE.vhd
@@ -2,8 +2,8 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity BLOCO_DE_CONTROLE is
-   port (S in std_logic_vector(1 downto 0);
-			BS,TT,F,: in  std_logic;
+   port (S: in std_logic_vector(1 downto 0);
+			BS,TT,F: in  std_logic;
 			N: out std_logic_vector(1 downto 0);
          led: out std_logic);
 end BLOCO_DE_CONTROLE;
@@ -21,9 +21,9 @@ C <= BS;
 D <= TT;
 E <= F;
    
-N(0) <= (NOT A and NOT B) or (NOT A and NOT C) or (NOT B and E) or (A and B);
+N(0) <= ((NOT A) and (NOT B)) or ((NOT A) and (NOT C)) or ((NOT B) and E) or (A and B);
 
-N(1)
+N(1) <= (A and (not E)) or ((not A) and B and C) or (A and (not B) and D);
 
 led <= (A and not(E)) or (not(A) and B and C) or (A and not(B) and D);
 	
