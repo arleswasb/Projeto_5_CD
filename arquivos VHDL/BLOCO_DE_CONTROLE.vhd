3bfc097264c62678305cf188737f70c7d8df5dd0
arleswasb <werbert.barradas.390@ufrn.edu.br>
2023-11-25 08:43:25 -0300

    Auto stash before merge of "main" and "projeto_5_mq_troco/main"

--- arquivos VHDL/BLOCO_DE_CONTROLE.vhd
+++ arquivos VHDL/BLOCO_DE_CONTROLE.vhd
@@ -2,25 +2,30 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity BLOCO_DE_CONTROLE is
-   port (BS, TT, F: in  std_logic;
-	 S: in std_logic_vector(1 downto 0);
+   port (S in std_logic_vector(1 downto 0);
+			BS,TT,F,: in  std_logic;
 			N: out std_logic_vector(1 downto 0);
          led: out std_logic);
 end BLOCO_DE_CONTROLE;
 
 architecture CKT of BLOCO_DE_CONTROLE is
-signal A, B, C, D, F, saida: std_logic;
+
+signal A,B,C,D,E : STD_LOGIC;
+
 
 begin
+
 A <= S(1);
 B <= S(0);
 C <= BS;
 D <= TT;
 E <= F;
+   
+N(0) <= (NOT A and NOT B) or (NOT A and NOT C) or (NOT B and E) or (A and B);
 
-N(0) <= not(A and B) or (A and C) or (B and E) or (A and B);
+N(1)
 
-saida <= (A and not(E)) or (not(A) and B and C) or (A and not(B) and D);
-led <= saida;	
-N(1) <= saida;	
+led <= (A and not(E)) or (not(A) and B and C) or (A and not(B) and D);
+	
+	
 end CKT;
