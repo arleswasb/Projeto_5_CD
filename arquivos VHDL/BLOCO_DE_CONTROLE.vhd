f0b46d273218bc53b3c44d7b1c3b222c1acf7f68
Rafael Franco <53819480+rafael2673@users.noreply.github.com>
2023-11-25 08:40:54 -0300

    Corrigindo entradas

--- arquivos VHDL/BLOCO_DE_CONTROLE.vhd
+++ arquivos VHDL/BLOCO_DE_CONTROLE.vhd
@@ -2,19 +2,25 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity BLOCO_DE_CONTROLE is
-   port (A, B, C, D, E: in  std_logic;
+   port (BS, TT, F: in  std_logic;
+	 S: in std_logic_vector(1 downto 0);
 			N: out std_logic_vector(1 downto 0);
          led: out std_logic);
 end BLOCO_DE_CONTROLE;
 
 architecture CKT of BLOCO_DE_CONTROLE is
-
+signal A, B, C, D, F, saida: std_logic;
 
 begin
-   
-N <= not(A and B) or (A and C) or (B and E) or (A and B);
+A <= S(1);
+B <= S(0);
+C <= BS;
+D <= TT;
+E <= F;
 
-led <= (A and not(E)) or (not(A) and B and C) or (A and not(B) and D);
-	
-	
+N(0) <= not(A and B) or (A and C) or (B and E) or (A and B);
+
+saida <= (A and not(E)) or (not(A) and B and C) or (A and not(B) and D);
+led <= saida;	
+N(1) <= saida;	
 end CKT;
